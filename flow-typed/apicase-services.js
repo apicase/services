762326175789b6c553f0014a36ae9f2b29db09c7
Kelin2025 <kelin2025@yandex.ru>
2017-10-15 02:53:45 +0300

    Fixed npm publishing + added types declarations

--- flow-typed/apicase-services.js
+++ flow-typed/apicase-services.js
@@ -1,27 +1,21 @@
-declare module 'apicase-core' {
+declare module 'apicase-services' {
+  declare type HooksT<T> = {
+    [hookType: string]: T
+  }
 
-  // Hooks
   declare type Hook = (ctx: Object, next: Function) => void
 
-  declare type HooksObject = {
-    [hookType: string]: Hook[]
-  }
+  declare type HooksObject = HooksT<Hook[]>
 
-  declare type UnnormalizedHooksObject = {
-    [hookType: string]: Hook | Hook[]
-  }
+  declare type UnnormalizedHooksObject = HooksT<Hook | Hook[]>
 
-  // Events
   declare type EventName = 'before' | 'start' | 'success' | 'error' | 'finish' | 'preinstall' | 'postinstall'
 
-  // Options
   declare type AllOptions = {
     adapter?: string,
-    hooks?: UnnormalizedHooksObject,
-    [string]: any
+    hooks?: UnnormalizedHooksObject
   }
 
-  // Adapters
   declare type Adapter<Options> = (query: {
     options: Options,
     done: (data: mixed) => void,
@@ -29,10 +23,8 @@ declare module 'apicase-core' {
     another: (hookType: string, data: mixed, reject?: boolean) => void
   }) => void
 
-  // Plugins
   declare type Plugin = (instance: Apicase) => void
 
-  // Instance
   declare type Apicase = {
     base: {
       query: Object,
@@ -48,12 +40,11 @@ declare module 'apicase-core' {
     call: (options: AllOptions) => Promise<mixed>,
     all: (options: AllOptions[]) => Promise<mixed>,
     of: (options: AllOptions) => Apicase,
-    install: (installer: (instance: Apicase) => void) => void,
-    extend: (installer: (instance: Apicase) => void) => Apicase,
+    install: (installer: Plugin) => void,
+    extend: (installer: Plugin) => Apicase,
     on: (event: EventName, callback: (...args: any[]) => void) => void,
     // For plugins
     [string]: any
   }
-
-  declare module.exports: Apicase;
+  declare module.exports: (installer: Plugin) => void;
 }
