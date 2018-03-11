a4bd5d52b43f2dcea8c13a945d2bcf543a624f07
Kelin2025 <kelin2025@yandex.ru>
2018-03-12 02:53:35 +0300

    Temporary removed tests

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const { ApiTree } = require('../index.cjs.js')
-const fetch = require('@apicase/adapter-fetch')
-
-console.log(
-  new ApiTree(fetch, [
-    { name: 'hello', url: 'test' },
-    { name: 'helloAgain', url: 'kek' }
-  ])
-)
