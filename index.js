b51ab4dd91d7d0d2106ee92bd8cd09e0a44d7910
Anton Kosykh <kelin2025@yandex.ru>
2018-03-26 16:05:59 +0300

    Removed this. + changed `omitz import from cjs folder + format

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import { omit } from 'nanoutils'
-import { ApiService } from '@apicase/core'
+import omit from "nanoutils/cjs/omit"
+import { ApiService } from "@apicase/core"
 
-const getOpts = omit(['name', 'on', 'children'])
+const getOpts = omit(["name", "on", "children"])
 
 export const ApiTree = function(base, items) {
-  this.items = items.reduce((res, item) => {
-    const next = base instanceof ApiService ? base.extend(getOpts(item)) : new ApiService(base, getOpts(item))
+  const services = items.reduce((res, item) => {
+    const next =
+      base instanceof ApiService
+        ? base.extend(getOpts(item))
+        : new ApiService(base, getOpts(item))
 
     if (item.on) {
       Object.entries(item.on).forEach(evt => next.on(evt[0], evt[1]))
@@ -17,21 +20,25 @@ export const ApiTree = function(base, items) {
     return res
   }, {})
 
-  return name => this.items[name]
+  return name => services[name]
 }
 
 const generateRestItem = {
-  getAll: name => ({ name: `${name}GetAll`, url: '', method: 'GET' }),
-  create: name => ({ name: `${name}Create`, url: '', method: 'POST' }),
-  getOne: name => ({ name: `${name}GetOne`, url: ':id', method: 'GET' }),
-  updOne: name => ({ name: `${name}UpdOne`, url: ':id', method: 'UPDATE' }),
-  rmvOne: name => ({ name: `${name}RmvOne`, url: ':id', method: 'DELETE' })
+  getAll: name => ({ name: `${name}GetAll`, url: "", method: "GET" }),
+  create: name => ({ name: `${name}Create`, url: "", method: "POST" }),
+  getOne: name => ({ name: `${name}GetOne`, url: ":id", method: "GET" }),
+  updOne: name => ({ name: `${name}UpdOne`, url: ":id", method: "UPDATE" }),
+  rmvOne: name => ({ name: `${name}RmvOne`, url: ":id", method: "DELETE" })
 }
 
 const defaultRest = Object.keys(generateRestItem)
 
 export const rest = (name, payload = defaultRest) =>
-  Array.isArray(payload) ? payload.map(key => generateRestItem[key](name)) : Object.entries(payload).map(item => Object.assign(generateRestItem[item[0]](name), item[1]))
+  Array.isArray(payload)
+    ? payload.map(key => generateRestItem[key](name))
+    : Object.entries(payload).map(item =>
+        Object.assign(generateRestItem[item[0]](name), item[1])
+      )
 
 export const wrappedRest = (name, payload = defaultRest) => ({
   url: name,
